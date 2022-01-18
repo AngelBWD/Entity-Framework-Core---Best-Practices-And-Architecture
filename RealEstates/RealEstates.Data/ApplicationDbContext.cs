1c119e4456ac75c236cc6f4ad2c55e436bf26800
beshirov <beshirov@DESKTOP-IL7R5L7>
2022-01-18 08:58:13 +0200

    Add ef configurations

---
+++ RealEstates/RealEstates.Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace RealEstates.Data
+{
+    public  class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions options)
+            :base(options)
+        {             
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                const string ConnectionString = "Server=DESKTOP-IL7R5L7\\SQLEXPRESS;Database=RealEstates;Integrated Security = true";
+                optionsBuilder.UseSqlServer(ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
